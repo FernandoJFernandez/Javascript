79d425d9d0b7a617d7581fa3db0ec2b6f50eb8d4
FernandoJFernandez <73706806+FernandoJFernandez@users.noreply.github.com>
2023-07-11 02:59:01 +0100

    Initial commit

---
+++ 7_Tecnología Ajax y uso de JSON/Vídeo 74/login2.php
@@ -0,0 +1,32 @@
+<?php
+
+$el_usuario=isset($_GET['usuario']) ? $_GET['usuario'] : $_POST['usuario'];
+$el_array = new stdClass();
+
+if ($el_usuario=="Juan") {
+  
+	$el_array->Nombre = "Juan";
+	$el_array->Apellido = "Gómez";
+	$el_array->Edad = "18";
+	$json = json_encode($el_array);
+	echo $json;
+}
+
+if ($el_usuario=="María") {
+  
+	$el_array->Nombre = "María";
+	$el_array->Apellido = "Núñez";
+	$el_array->Edad = "31";
+	$json = json_encode($el_array);
+	echo $json;
+}
+
+if ($el_usuario=="Fernando") {
+	$el_array->Nombre = "Fernando";
+	$el_array->Apellido = "Fernánadez";
+	$el_array->Edad = "39";
+	$json = json_encode($el_array);
+	echo $json;
+}
+
+?>
